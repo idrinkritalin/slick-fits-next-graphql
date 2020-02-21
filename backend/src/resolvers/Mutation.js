b240052a3d9cb5e3a1411f7223f5122da8a27427
idrinkritalin <stefano.veltri@blacklane.com>
2020-02-21 17:40:58 +0100

    * add the mutation for updateItem * refactor the query with forwardTo

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,8 +1,24 @@
+const { forwardTo } = require('prisma-binding')
+
 const Mutation = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem({
       data: { ...args }
     }, info)
+    return item
+  },
+
+  async updateItem(parent, args, ctx, info) {
+    const updates = { ...args }
+    delete updates.id
+
+    const item = await ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info)
+
     return item
   }
 }
